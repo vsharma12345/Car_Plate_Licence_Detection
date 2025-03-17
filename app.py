e7e7c9561366ce3e18f9f98e0e9343e540f6bf30
Vinay sharma <vinayjpr40@gmail.com>
2025-03-17 16:18:23 +0530

    changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import cv2
 import numpy as np
-from tensorflow.keras.models import load_model
+from tensorflow.keras.models import load_model 
 from PIL import Image
 
 # Load the trained model
